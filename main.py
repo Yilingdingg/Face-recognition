c9ee11e83171c6158ce9208a3d64ab1e5454e292
Yilingdingg <168896480+Yilingdingg@users.noreply.github.com>
2024-08-17 11:58:54 +0200

    Add files via upload

--- main.py
+++ main.py
@@ -3,15 +3,21 @@ import cv2
 face_xml="haarcascade_frontalface_default.xml"
 #detect face
 face_detection=cv2.CascadeClassifier(face_xml)
+path="datasets/Elise"
 #open camera
 webcam=cv2.VideoCapture(0)
-while True:
+count=1
+while count<31:
     ret, img=webcam.read()
     gray_img=cv2.cvtColor(img,cv2.COLOR_BGR2GRAY)
     face=face_detection.detectMultiScale(gray_img, 1.3, 4)
     print(face)
     for (x,y,w,h) in face:
         cv2.rectangle(img, (x,y),(x+w,y+h),(156,205,255),3)
+        person=gray_img[y:y+h,x:x+h]
+        person_resize=cv2.resize(person,(130,100))
+        cv2.imwrite(f"{path}/{count}.png", person_resize)
+    count+=1
     cv2.imshow("Webcam", img)
     key=cv2.waitKey(10)
     if key==27:
